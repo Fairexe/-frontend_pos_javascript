a423c4467cf3f6c3e5b2528d4aaf2946c16093de
62160052 <62160052@go.buu.ac.th>
2022-11-23 17:32:22 +0700

    Update pos.html product.html receipt.html and unit.html Now can view receipt from database

--- files/receipt.js
+++ files/receipt.js
@@ -1,16 +1,15 @@
-export async function addReceipt(code, fullTotal, discountTotal, subTotal, grandTotal, receiptDetail) {
+export async function addReceipt( fullTotal, discountTotal, subTotal, tradeDiscount, grandTotal, receiptDetail) {
     let options = {
         method: "POST",
         headers: {
-            "Accept": "application/json",
             "Content-Type": "application/json"
         }
     }
     let data = {
-        ReceiptCode: code,
         FullTotal: fullTotal,
         DiscountTotal: discountTotal,
         SubTotal: subTotal,
+        TradeDiscount: tradeDiscount,
         GrandTotal: grandTotal,
         ReceiptDetail: receiptDetail
     }
@@ -18,5 +17,18 @@ export async function addReceipt(code, fullTotal, discountTotal, subTotal, grand
     var url = "http://localhost:5154/receipt/AddReceipt"
     const response = await fetch(url, options)
         .then(res => res.json())
+    return response
+}
+
+export async function getReceipts() {
+    let options = {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    var url = "http://localhost:5154/receipt/GetReceipts"
+    const response = await fetch(url, options)
+    .then(res => res.json())
     return response
 }
